7da82d81cc0789500d7ced003c8904b451e2a67b
ronald <rchoquehuancar@unjbg.edu.pe>
2024-06-30 22:34:38 -0500

    ingresando programa para el ejercicio de rotar el vector

---
+++ rotar.cpp
@@ -0,0 +1,29 @@
+//Rotar las componentes de un vector una posición hacia la derecha.
+#include <iostream>
+#include <string.h>
+using namespace std;
+void rota(char cadena[],int largo){
+	char aux;
+	int tam=largo-1,i;
+	aux=cadena[tam];
+	for(i=0;i<largo;i++){
+		cadena[tam]=cadena[tam-1];
+		tam=tam-1;
+	}
+	
+	cadena[0]=aux;
+	for(i=0;i<largo;i++){
+		cout<<cadena[i];
+	}
+}
+
+int main(){
+	char cadena[100];
+	int largo;
+	cout<<"Ingrese solo una cadena de caracteres: ";
+	cin.getline(cadena,100,'\n');
+	largo=strlen(cadena);
+	
+	rota(cadena,largo);
+	return 0;
+}
